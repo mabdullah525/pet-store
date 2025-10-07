43eeddd201ab5a9b24e4f4b643cb28150d12bda1
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-10-07 01:12:51 -0700

    Conttext and provider ready

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -1,5 +1,7 @@
-import { createContext } from "react"; // 1. Import createContext from React
+import React from "react"; 
+import { createContext, useContext } from "react"; // 1. Import createContext from React
 const FirebaseContext = createContext(null); // 2. Create the context with a default value of null
+export const useFirebase = () => useContext(FirebaseContext); // 3. Create a custom hook to use the context
 
 export const FirebaseProvider = ({ children }) => {
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { FirebaseProvider } from './context/Firebase.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <FirebaseProvider>
+      <App />
+    </FirebaseProvider>
   </StrictMode>,
 )
